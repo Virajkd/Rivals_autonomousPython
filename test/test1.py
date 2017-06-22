c1f2fa820ab49dd1342a0b7774170adb565706cd
Viraj Kaushalye <virajkaush@gmail.com>
2017-06-22 09:59:09 -0700

    get screen garb to cv2 window

--- test/test1.py
+++ test/test1.py
@@ -2,6 +2,10 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
-def void Screen_Grab(){
-
-}
+while True:
+    printscreen_pil = ImageGrab.grab(bbox=(0,40,800,640))
+    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8').reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+    cv2.imshow('window', printscreen_numpy)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
